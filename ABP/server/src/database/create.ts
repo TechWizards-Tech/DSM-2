003742308cb1bf3210c6b494b3dfb4edefcc7f27
turnupthetaste <161386296+turnupthetaste@users.noreply.github.com>
2024-10-25 17:39:46 -0300

    Update create.ts

--- ABP/server/src/database/create.ts
+++ ABP/server/src/database/create.ts
@@ -36,123 +36,115 @@ async function init() {
                 ON UPDATE CASCADE
         );
 
+        CREATE TABLE fields (
+            id SERIAL NOT NULL,
+            name VARCHAR(50) NULL,
+            unit VARCHAR(5) NULL,
+            field VARCHAR(30) NULL,
+            PRIMARY KEY(id)
+        );
 
-        
-CREATE TABLE IF NOT EXISTS public.categories
-(
-    id serial NOT NULL,
-    name character varying(40) COLLATE pg_catalog."default",
-    CONSTRAINT categories_pkey PRIMARY KEY (id)
-);
+        CREATE TABLE categories (
+            id SERIAL NOT NULL,
+            name VARCHAR(40) NULL,
+            PRIMARY KEY(id)
+        );
 
-CREATE TABLE IF NOT EXISTS public.diet
-(
-    idd serial NOT NULL,
-    diet integer NOT NULL,
-    objective integer NOT NULL,
-    text character varying(45) COLLATE pg_catalog."default" NOT NULL,
-    period integer NOT NULL,
-    CONSTRAINT diet_pkey PRIMARY KEY (idd)
-);
+        CREATE TABLE foods (
+            id SERIAL NOT NULL,
+            category INTEGER NOT NULL,
+            description VARCHAR(70) NULL,
+            moisture FLOAT NULL,
+            energy FLOAT NULL,
+            protein FLOAT NULL,
+            lipids FLOAT NULL,
+            cholesterol FLOAT NULL,
+            carbohydrate FLOAT NULL,
+            dietary_fiber FLOAT NULL,
+            ash FLOAT NULL,
+            calcium FLOAT NULL,
+            magnesium FLOAT NULL,
+            manganese FLOAT NULL,
+            phosphorus FLOAT NULL,
+            iron FLOAT NULL,
+            sodium FLOAT NULL,
+            potassium FLOAT NULL,
+            copper FLOAT NULL,
+            zinc FLOAT NULL,
+            retinol FLOAT NULL,
+            re FLOAT NULL,
+            era FLOAT NULL,
+            thiamin FLOAT NULL,
+            riboflavin FLOAT NULL,
+            pyridoxine FLOAT NULL,
+            niacin FLOAT NULL,
+            vitamin_c FLOAT NULL,
+            PRIMARY KEY(id),
+            FOREIGN KEY(category)
+                REFERENCES categories(id)
+                ON DELETE RESTRICT
+                ON UPDATE CASCADE
+        );
 
-CREATE TABLE IF NOT EXISTS public.eat_foods
-(
-    id serial NOT NULL,
-    _user integer NOT NULL,
-    food integer NOT NULL,
-    date date,
-    quantity double precision,
-    period date,
-    CONSTRAINT eat_foods_pkey PRIMARY KEY (id)
-);
+        CREATE TABLE products (
+            id SERIAL NOT NULL,
+            _user INTEGER NOT NULL,
+            description VARCHAR(50) NOT NULL,
+            serving_size FLOAT NOT NULL,
+            serving_size_unit VARCHAR(10) NOT NULL,
+            quantity_per_serving FLOAT NULL,
+            quantity_per_serving_unit VARCHAR(20) NULL,
+            energy FLOAT NULL,
+            protein FLOAT NULL,
+            carbohydrate FLOAT NULL,
+            sugar FLOAT NULL,
+            dietary_fiber FLOAT NULL,
+            total_fat FLOAT NULL,
+            saturated_fat FLOAT NULL,
+            trans_fat FLOAT NULL,
+            calcium FLOAT NULL,
+            sodium FLOAT NULL,
+            PRIMARY KEY(id),
+            FOREIGN KEY(_user)
+                REFERENCES users(id)
+                ON DELETE RESTRICT
+                ON UPDATE CASCADE
+        );
 
-CREATE TABLE IF NOT EXISTS public.foods
-(
-    id serial NOT NULL,
-    category integer NOT NULL,
-    description character varying(70) COLLATE pg_catalog."default",
-    moisture double precision,
-    energy double precision,
-    protein double precision,
-    lipids double precision,
-    cholesterol double precision,
-    carbohydrate double precision,
-    dietary_fiber double precision,
-    ash double precision,
-    calcium double precision,
-    magnesium double precision,
-    manganese double precision,
-    phosphorus double precision,
-    iron double precision,
-    sodium double precision,
-    potassium double precision,
-    copper double precision,
-    zinc double precision,
-    retinol double precision,
-    re double precision,
-    era double precision,
-    thiamin double precision,
-    riboflavin double precision,
-    pyridoxine double precision,
-    niacin double precision,
-    vitamin_c double precision,
-    CONSTRAINT foods_pkey PRIMARY KEY (id)
-);
+        CREATE TABLE eat_foods (
+            id SERIAL NOT NULL,
+            _user INTEGER NOT NULL,
+            food INTEGER NOT NULL,
+            date DATE NULL,
+            quantity FLOAT NULL,
+            PRIMARY KEY(id),
+            FOREIGN KEY(food)
+                REFERENCES foods(id)
+                ON DELETE RESTRICT
+                ON UPDATE CASCADE,
+            FOREIGN KEY(_user)
+                REFERENCES users(id)
+                ON DELETE RESTRICT
+                ON UPDATE CASCADE
+        );
 
-CREATE TABLE IF NOT EXISTS public.profiles
-(
-    id serial NOT NULL,
-    _user integer NOT NULL,
-    birth_date date NOT NULL,
-    weight double precision NOT NULL,
-    sex enum_sex,
-	height double precision NOT NULL,
-	ideal_weight double precision,
-	diet smallint NOT NULL,
-	objective smallint NOT NULL,
-	CONSTRAINT profiles_pkey PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS public.users
-(
-    id serial NOT NULL,
-    alias character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    mail character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    password character varying(100) COLLATE pg_catalog."default",
-    role enum_role NOT NULL DEFAULT 'user'::enum_role,
-    CONSTRAINT users_pkey PRIMARY KEY (id),
-    CONSTRAINT users_mail_unique UNIQUE (mail)
-);
-
-ALTER TABLE IF EXISTS public.eat_foods
-    ADD CONSTRAINT eat_foods__user_fkey FOREIGN KEY (_user)
-    REFERENCES public.users (id) MATCH SIMPLE
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT;
-
-
-ALTER TABLE IF EXISTS public.eat_foods
-    ADD CONSTRAINT eat_foods_food_fkey FOREIGN KEY (food)
-    REFERENCES public.foods (id) MATCH SIMPLE
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT;
-
-
-ALTER TABLE IF EXISTS public.foods
-    ADD CONSTRAINT foods_category_fkey FOREIGN KEY (category)
-    REFERENCES public.categories (id) MATCH SIMPLE
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT;
-
-
-ALTER TABLE IF EXISTS public.profiles
-    ADD CONSTRAINT profiles__user_fkey FOREIGN KEY (_user)
-    REFERENCES public.users (id) MATCH SIMPLE
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT;
-
-
-            END;
+        CREATE TABLE eat_products (
+            id SERIAL NOT NULL,
+            _user INTEGER NOT NULL,
+            product INTEGER NOT NULL,
+            date DATE NULL,
+            quantity FLOAT NULL,
+            PRIMARY KEY(id),
+            FOREIGN KEY(_user)
+                REFERENCES users(id)
+                ON DELETE RESTRICT
+                ON UPDATE CASCADE,
+            FOREIGN KEY(product)
+                REFERENCES products(id)
+                ON DELETE RESTRICT
+                ON UPDATE CASCADE
+        );
+    END;
     $$;
     `);
     // Verifique se a tabela foi criada
