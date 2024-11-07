a0f0c0b0abc6d4d201ac38c2996d18f957df0481
Felipe <felipecorrea71@gmail.com>
2024-11-07 11:03:09 -0300

    Updated services

--- ABP/frontend/src/services/Food.ts
+++ ABP/frontend/src/services/Food.ts
@@ -1,43 +1,67 @@
 import { ErrorProps, FoodNutrientsProps, PageProps } from "../types";
 import { api } from "./api";
 
-class Food {
+class FoodService {
+  // Método para listar os alimentos com paginação
   async list(page: number): Promise<PageProps | ErrorProps> {
     const params = {
       page,
       pagesize: process.env.REACT_APP_PAGE_SIZE,
     };
-
     try {
-      const { data } = await api.get("/food/list", { params });
+      const token = localStorage.getItem("userToken");
+      const { data } = await api.get<PageProps>("/food/list", {
+        params,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       return data;
     } catch (error: any) {
-      return error;
+      return this.handleError(error);
     }
   }
 
+  // Método para obter detalhes de um alimento pelo ID
   async getById(idfood: string): Promise<FoodNutrientsProps | ErrorProps> {
-    const params = {idfood};
-
+    const params = { idfood };
     try {
-      const { data } = await api.get("/food/get", { params });
+      const token = localStorage.getItem("userToken");
+      const { data } = await api.get<FoodNutrientsProps>("/food/get", {
+        params,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       return data;
     } catch (error: any) {
-      return error;
+      return this.handleError(error);
     }
   }
 
+  // Método para buscar alimentos com base em um termo
   async search(term: string): Promise<PageProps | ErrorProps> {
-    const params = {term};
-
+    const params = { term };
     try {
-      const { data } = await api.get("/food/search", { params });
+      const token = localStorage.getItem("userToken");
+      const { data } = await api.get<PageProps>("/food/search", {
+        params,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
       return data;
     } catch (error: any) {
-      return error;
+      return this.handleError(error);
     }
+  }
+
+  // Função dedicada para lidar com erros
+  private handleError(error: any): ErrorProps {
+    console.error("Erro ao acessar a API:", error);
+    return { error: error.message || "Erro desconhecido ao acessar a API." };
   }
 }
 
-const food = new Food();
-export default food;
+const foodService = new FoodService();
+export default foodService;
