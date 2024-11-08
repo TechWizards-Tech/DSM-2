48e4a08e298f928736cb156c8360bc7b858648e4
gmlebc <gmlebc@gmail.com>
2024-11-07 21:24:15 -0300

    modificaçoes

--- ABP/frontend/src/FoodForm.tsx
+++ ABP/frontend/src/FoodForm.tsx
@@ -3,71 +3,9 @@ import './FoodForm.css';
 import { CalendarCheck } from 'lucide-react';
 import Button from './Button';
 
-interface FoodFormProps {
-    onAddRecord: (record: { food: string; date: string; amount: string; meal: string }) => void;
-}
 
-const FoodForm: React.FC<FoodFormProps> = ({ onAddRecord }) => {
+const FoodForm: React.FC = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const foodList = [
-        'Arroz carreteiro',
-        'Arroz, integral, cozido',
-        'Arroz, integral, cru',
-        'Arroz, tipo 1, cozido',
-        'Arroz, tipo 1, cru',
-        'Arroz, tipo 2, cozido',
-        'Arroz, tipo 2, cru',
-        'Baião de dois, arroz e feijão-de-corda',
-        'Bolinho de arroz',
-        'Creme de arroz, pó',
-        'Farinha, de arroz, enriquecida'
-    ];
-
-    const [searchTerm, setSearchTerm] = useState('');
-    const [filteredFoods, setFilteredFoods] = useState<string[]>([]);
-    const [selectedFood, setSelectedFood] = useState<string | null>(null);
-    const [consumptionDate, setConsumptionDate] = useState('');
-    const [consumedAmount, setConsumedAmount] = useState('');
-    const [mealType, setMealType] = useState('');
-
-    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const search = e.target.value;
-        setSearchTerm(search);
-        if (search) {
-            const filtered = foodList.filter((food) =>
-                food.toLowerCase().includes(search.toLowerCase())
-            );
-            setFilteredFoods(filtered);
-        } else {
-            setFilteredFoods([]);
-        }
-    };
-
-    const handleSelectFood = (food: string) => {
-        setSelectedFood(food);
-        setSearchTerm(food);
-        setFilteredFoods([]);
-    };
-
-    const handleSave = () => {
-        if (selectedFood && consumptionDate && consumedAmount && mealType) {
-            const newRecord = {
-                food: selectedFood,
-                date: consumptionDate,
-                amount: consumedAmount,
-                meal: mealType,
-            };
-            onAddRecord(newRecord);
-            setSearchTerm('');
-            setSelectedFood(null);
-            setConsumptionDate('');
-            setConsumedAmount('');
-            setMealType('');
-        } else {
-            alert('Por favor, preencha todos os campos.');
-        }
-    };
 
     return (
         <div className={isModalOpen ? 'blurred-background' : 'auth-button-meal'} style={{ backgroundColor: 'transparent' }}>
@@ -88,52 +26,31 @@ const FoodForm: React.FC<FoodFormProps> = ({ onAddRecord }) => {
                         <label>Busca alimento ou produto consumido:</label>
                         <input
                             className='input-custom'
-                            type="text"
-                            value={searchTerm}
-                            onChange={handleSearchChange}
+                            type="text"      
                             placeholder="Digite para buscar"
                         />
-                        {filteredFoods.length > 0 && (
+                       
                             <ul className="dropdown">
-                                {filteredFoods.map((food, index) => (
-                                    <li
-                                        key={index}
-                                        className='dropli'
-                                        onClick={() => handleSelectFood(food)}
-                                        style={{
-                                            backgroundColor: selectedFood === food ? '#4b7e56' : '',
-                                            color: selectedFood === food ? '#f2f7f3' : ''
-                                        }}
-                                    >
-                                        {food}
-                                    </li>
-                                ))}
+                                    <li></li>
                             </ul>
-                        )}
+                    
 
                         <label>Data de consumo:</label>
                         <input
                             className='input-custom'
                             type="date"
-                            value={consumptionDate}
-                            onChange={(e) => setConsumptionDate(e.target.value)}
                         />
 
                         <label>Quantidade consumida:</label>
                         <input
                             className='input-custom'
                             type="text"
-                            value={consumedAmount}
-                            onChange={(e) => setConsumedAmount(e.target.value)}
                             placeholder="Ex: 3 colheres"
                         />
 
                         <label>Periodo:</label>
-                        <select
-                            className='input-custom'
-                            value={mealType}
-                            onChange={(e) => setMealType(e.target.value)}
-                        >
+                        <select className='input-custom'>
+                        
                             <option value="">Selecione</option>
                             <option value="Cafe da manha">Café da manhã</option>
                             <option value="Almoço">Almoço</option>
@@ -141,7 +58,7 @@ const FoodForm: React.FC<FoodFormProps> = ({ onAddRecord }) => {
                             <option value="Janta">Janta</option>
                         </select>
 
-                        <Button label={'Salvar'} onClick={handleSave} className='auth-buttonfont-style: italic;'/>
+                        <Button label={'Salvar'} className='auth-buttonfont-style: italic;'/>
                     </div>
                 </div>
             )}
--- ABP/frontend/src/MealCard.tsx
+++ ABP/frontend/src/MealCard.tsx
@@ -6,13 +6,10 @@ interface MealCardProps {
     time: string;
     mealName: string;
     caloriesConsumed: number;
-    totalCalories: number;
-    consumptionRecords: { food: string; date: string; amount: string; meal: string }[];
+    period: number;
 }
 
-const MealCard: React.FC<MealCardProps> = ({ time, mealName, caloriesConsumed, totalCalories, consumptionRecords }) => {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const progress = (caloriesConsumed / totalCalories) * 100;
+const MealCard: React.FC <MealCardProps> = () => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -24,43 +21,33 @@ const MealCard: React.FC<MealCardProps> = ({ time, mealName, caloriesConsumed, t
     return (
         <div className="meal-card bg-green-100">
             <div className="meal-info">
-                <p className="meal-time">{time}</p>
-                <p className="meal-name">{mealName}</p>
-                <p className="meal-calories">
-                    {caloriesConsumed}/{totalCalories} kcal
-                </p>
+                <p className="meal-time">08:00</p>
+                <p className="meal-name">cafe</p>
+                <p className="meal-calories">10kcal</p>
             </div>
 
             <div className="progress-bar-container">
-                <div className="progress-bar" style={{ width: `${progress}%` }}></div>
+                <div className="progress-bar"></div>
             </div>
 
             <div className="add-period-container" style={{ backgroundColor: 'transparent' }}>
-            <Clock
-                className="timer-svg svg-stroke-green-700"
-                onClick={toggleModal}
-                style={{ cursor: 'pointer' }}
-            />
+                <Clock
+                    className="timer-svg svg-stroke-green-700"
+                    onClick={toggleModal}
+                    style={{ cursor: 'pointer' }}
+                />
                 {isModalOpen && (
-                <div className="modal-overlay" onClick={toggleModal}>
-                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                        <span className="close-button" onClick={toggleModal}>&times;</span>
-                        <h3>Registro de Consumo</h3>
-                        <ul className="modal-list">
-                            {consumptionRecords.map((record, index) => (
-                                <li key={index}>
-                                    {record.food} - {record.amount} - {record.date} - {record.meal}
-                                </li>
-                            ))}
-                        </ul>
+                    <div className="modal-overlay" onClick={toggleModal}>
+                        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                            <span className="close-button" onClick={toggleModal}>&times;</span>
+                            <h3>Registro de Consumo</h3>
+                            <ul className="modal-list">
+                                <li> </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
             </div>
-
-            {/* <div className="add-button-container auth-button-meal">
-                <CirclePlus className='timer-svg svg-stroke-green-700' />
-            </div> */}
         </div>
     );
 };
